9076968b61fc8095d0c29606e7ef8f6300a34519
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2024-07-29 09:29:13 +0000

    function for filtering recipe

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ const recipeObject = [
       { NAME: "Slipped Almonds", AMOUNT: "50 g" },
     ],
     description: "Mix everything, heat it, and you are good to go!",
+  },{
+    id: 2,
+    title: "ABC",
+    picture_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Gl%C3%B6gg_kastrull.JPG/800px-Gl%C3%B6gg_kastrull.JPG",
+    ingredients: [
+      { NAME: "Orange zest", AMOUNT: "0.5" },
+      { NAME: "Water", AMOUNT: "200 ml" },
+      { NAME: "Sugar", AMOUNT: "275 g" },
+      { NAME: "Whole cloves", AMOUNT: "5" },
+      { NAME: "Cinnamon sticks", AMOUNT: "2" },
+      { NAME: "Spice", AMOUNT: undefined },
+      { NAME: "Bottle of red wine", AMOUNT: "1" },
+      { NAME: "Raisins", AMOUNT: "100 g" },
+      { NAME: "Slipped Almonds", AMOUNT: "50 g" },
+    ],
+    description: "Mix everything, heat it, and you are good to go!",
   },
 ];
 
@@ -198,3 +215,12 @@ const text3 = "Join our community of food enthusiasts and embark on a culinary a
     window.onload = function() {
       typeWriter1();
     }
+
+    // Recipe Search function
+
+    function findRecipe(){
+      const searchKey = document.getElementById("searchkey").value.toLowerCase();
+      const filteredRecipes = recipeObject.filter(recipe =>recipe.title.toLowerCase().includes(searchKey))
+      const title = filteredRecipes.title
+      console.log(title)
+  }
