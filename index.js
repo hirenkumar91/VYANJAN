d997ff7060794d0496ac7df2f880d759330b6f39
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2024-08-02 07:39:27 +0000

    fatch data

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-const recipeObject = [
-  {
-    id: 1,
-    title: "Gløgg",
-    picture_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Gl%C3%B6gg_kastrull.JPG/800px-Gl%C3%B6gg_kastrull.JPG",
-    ingredients: [
-      { NAME: "Orange zest", AMOUNT: "0.5" },
-      { NAME: "Water", AMOUNT: "200 ml" },
-      { NAME: "Sugar", AMOUNT: "275 g" },
-      { NAME: "Whole cloves", AMOUNT: "5" },
-      { NAME: "Cinnamon sticks", AMOUNT: "2" },
-      { NAME: "Spice", AMOUNT: "" },
-      { NAME: "Bottle of red wine", AMOUNT: "1" },
-      { NAME: "Raisins", AMOUNT: "100 g" },
-      { NAME: "Slipped Almonds", AMOUNT: "50 g" },
-    ],
-    description: "Mix everything, heat it, and you are good to go!",
-  },
-  {
-    id: 2,
-    title: "ABC",
-    picture_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Gl%C3%B6gg_kastrull.JPG/800px-Gl%C3%B6gg_kastrull.JPG",
-    ingredients: [
-      { NAME: "Orange zest", AMOUNT: "0.5" },
-      { NAME: "Water", AMOUNT: "200 ml" },
-      { NAME: "Sugar", AMOUNT: "275 g" },
-      { NAME: "Whole cloves", AMOUNT: "5" },
-      { NAME: "Cinnamon sticks", AMOUNT: "2" },
-      { NAME: "Spice", AMOUNT: "" },
-      { NAME: "Bottle of red wine", AMOUNT: "1" },
-      { NAME: "Raisins", AMOUNT: "100 g" },
-      { NAME: "Slipped Almonds", AMOUNT: "50 g" },
-    ],
-    description: "Mix everything, heat it, and you are good to go!",
-  },
-];
+const dataUrl = "https://raw.githubusercontent.com/hirenkumar91/mYapi/main/recipiAppdata/data.json",
+const recipeObject = [];
+
+async function fetchRecipes() {
+  try {
+      const response = await fetch(dataUrl);
+      recipeObject = await response.json();
+      displayRecipes(recipeObject);
+  } catch (error) {
+      console.error('Error fetching recipes:', error);
+  }
+}
 
 let addCount = 0;
 
